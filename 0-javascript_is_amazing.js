bb008ce3570ea1348a51d03df330a66ed1855f4d
Abdulraman Saumu <abdulrahmansaumu@gmail.com>
2025-05-30 21:24:44 +0300

    correcting 0-javascript_is _amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,2 @@
-const myVar = 'JavaScript is amazing';  // Create a constant variable with the value 'JavaScript is amazing'
+const myVar = 'JavaScript is amazing';
 console.log(myVar);              
